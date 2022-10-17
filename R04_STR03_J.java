1b3792aadb79f0d1305d08d382aa872f4dcd55c6
Priyansh Patel <72054012+ppriyansh2000@users.noreply.github.com>
2022-10-17 14:37:36 -0400

    Update R04_STR03_J.java
    Added Compliant Solution

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,9 +1,9 @@
 // Rule 04.
 // Characters and Strings (STR)
-// Non Compliant Solution
+// Compliant Solution
 
 BigInteger x = new BigInteger("530500452766");
-byte[] byteArray = x.toByteArray();
-String s = new String(byteArray);
-byteArray = s.getBytes();
-x = new BigInteger(byteArray);
+String s = x.toString();  // Valid character data
+byte[] byteArray = s.getBytes();
+String ns = new String(byteArray); 
+x = new BigInteger(ns);
