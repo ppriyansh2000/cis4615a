7c7bd65ea5ecb8dd1aa6a094fa51494f87a8fa53
Priyansh Patel <72054012+ppriyansh2000@users.noreply.github.com>
2022-10-17 14:38:36 -0400

    Update R06_MET01_J.java
    Added Compliant Solution

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,14 +1,15 @@
 // Rule 06.
 // Methods (MET)
-// Non Compliant Solution
+// Compliant Solution
 
 public static int getAbsAdd(int x, int y) {
-    assert x != Integer.MIN_VALUE;
-    assert y != Integer.MIN_VALUE;
-    int absX = Math.abs(x);
-    int absY = Math.abs(y);
-    assert (absX <= Integer.MAX_VALUE - absY);
-    return absX + absY;
+  if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+    throw new IllegalArgumentException();
   }
-
-  //Usage: getAbsAdd(Integer.MIN_VALUE, 1);
+  int absX = Math.abs(x);
+  int absY = Math.abs(y);
+  if (absX > Integer.MAX_VALUE - absY) {
+    throw new IllegalArgumentException();
+  }
+  return absX + absY;
+}
