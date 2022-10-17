7a1095c735b96187716d866b906a9a3e04b6e59b
Priyansh Patel <72054012+ppriyansh2000@users.noreply.github.com>
2022-10-17 14:41:44 -0400

    Update R13_FIO01_J.java
    Added Compliant Solution

--- R13_FIO01_J.java
+++ R13_FIO01_J.java
@@ -1,5 +1,21 @@
 // Rule 13.
 // Input Output (FIO)
-// Non Compliant Solution
+// Compliant Solution
 
-Writer out = new FileWriter("file");
+Path file = new File("file").toPath();
+ 
+// Throw exception rather than overwrite existing file
+Set<OpenOption> options = new HashSet<OpenOption>();
+options.add(StandardOpenOption.CREATE_NEW);
+options.add(StandardOpenOption.APPEND);
+ 
+// File permissions should be such that only user may read/write file
+Set<PosixFilePermission> perms =
+    PosixFilePermissions.fromString("rw-------");
+FileAttribute<Set<PosixFilePermission>> attr =
+    PosixFilePermissions.asFileAttribute(perms);
+ 
+try (SeekableByteChannel sbc =
+         Files.newByteChannel(file, options, attr)) {
+  // Write data
+};
