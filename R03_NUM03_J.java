c8bb6bed4571032b64d353c8cae98835dd013b8f
Priyansh Patel <72054012+ppriyansh2000@users.noreply.github.com>
2022-10-17 14:36:13 -0400

    Update R03_NUM03_J.java
    Added Compliant Solution

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,7 +1,7 @@
 // Rule 03.
 // Numeric Types and operations (NUM)
-// Non Compliant Solution
+// Compliant Solution
 
-public static int getInteger(DataInputStream is) throws IOException {
-    return is.readInt(); 
-  }
+public static long getInteger(DataInputStream is) throws IOException {
+  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
+}
