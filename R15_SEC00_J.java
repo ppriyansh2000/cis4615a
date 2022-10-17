1bb8b66cff825a4b77c84161b8f768982861108b
Priyansh Patel <72054012+ppriyansh2000@users.noreply.github.com>
2022-10-17 14:44:17 -0400

    Update R15_SEC00_J.java
    Added Compliant Solution

--- R15_SEC00_J.java
+++ R15_SEC00_J.java
@@ -1,37 +1,33 @@
 // Rule 15.
 // Platform Security (SEC)
-// Non Compliant Solution
+// Compliant Solution
 
-public class PasswordManager {
+class PasswordManager {
  
-    public static void changePassword() throws FileNotFoundException {
-      FileInputStream fin = openPasswordFile();
-   
-      // Test old password with password in file contents; change password,
-      // then close the password file
+  public static void changePassword() {
+    FileInputStream fin = openPasswordFile();
+    if (fin == null) {
+      // No password file; handle error
     }
-   
-    public static FileInputStream openPasswordFile()
-        throws FileNotFoundException {
-      final String password_file = "password";
-      FileInputStream fin = null;
-      try {
-        fin = AccessController.doPrivileged(
-          new PrivilegedExceptionAction<FileInputStream>() {
-            public FileInputStream run() throws FileNotFoundException {
-              // Sensitive action; can't be done outside privileged block
-              FileInputStream in = new FileInputStream(password_file);
-              return in;
-            }
-        });
-      } catch (PrivilegedActionException x) {
-        Exception cause = x.getException();
-        if (cause instanceof FileNotFoundException) {
-          throw (FileNotFoundException) cause;
-        } else {
-          throw new Error("Unexpected exception type", cause);
-        }
-      }
-      return fin;
-    }
+ 
+    // Test old password with password in file contents; change password
   }
+ 
+  private static FileInputStream openPasswordFile() {
+    final String password_file = "password";
+    final FileInputStream fin[] = { null };
+    AccessController.doPrivileged(new PrivilegedAction<Void>() {
+        public Void run() {
+          try {
+            // Sensitive action; can't be done outside
+            // doPrivileged() block
+            fin[0] = new FileInputStream(password_file);
+          } catch (FileNotFoundException x) {
+            // Report to handler
+          }
+          return null;
+        }
+    });
+    return fin[0];
+  }
+}
