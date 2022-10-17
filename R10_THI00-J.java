6ef890e538f3131265ea12b0b93ea79a68225e70
Priyansh Patel <72054012+ppriyansh2000@users.noreply.github.com>
2022-10-17 14:41:05 -0400

    Update R10_THI00-J.java
    Added Compliant Solution

--- R10_THI00-J.java
+++ R10_THI00-J.java
@@ -1,14 +1,14 @@
 // Rule 10.
 // Thread APIs (THI)
-// Non Compliant Solution
+// Compliant Solution
 
 public final class Foo implements Runnable {
-    @Override public void run() {
-      // ...
-    }
-   
-    public static void main(String[] args) {
-      Foo foo = new Foo();
-      new Thread(foo).run();
-    }
+  @Override public void run() {
+    // ...
   }
+ 
+  public static void main(String[] args) {
+    Foo foo = new Foo();
+    new Thread(foo).start();
+  }
+}
